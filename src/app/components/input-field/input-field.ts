ecbf80cb153f2844e38752c169080c2267ffeba0
desteban <esteban28ec@outlook.com>
2025-09-16 21:41:18 -0500

    componente input

---
+++ src/app/components/input-field/input-field.ts
@@ -0,0 +1,78 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+export enum InputFieldVariants {
+  'text' = 'text',
+  'password' = 'password',
+  'email' = 'email',
+  'number' = 'number',
+  'phone' = 'phone',
+}
+
+@Component({
+  selector: 'component-input-field',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './input-field.html',
+  styleUrl: './input-field.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputField),
+      multi: true,
+    },
+  ],
+})
+export class InputField implements ControlValueAccessor {
+  @Input({ required: true }) id!: string;
+  @Input({ required: false }) name: string | undefined = undefined;
+  @Input({ required: true }) placeholder!: string;
+  @Input({ required: false }) label: string = '';
+  @Input({ required: false }) type: InputFieldVariants = InputFieldVariants.text;
+  @Input({ required: false }) error: string | undefined = undefined;
+  @Input({ required: false }) required: boolean = false;
+  @Input({ required: false }) helpText: string | undefined = undefined;
+
+  private _currentValue: string = '';
+  private _disabled: boolean = false;
+  private onChange = (value: any): void => {};
+  private onTouched = (value: any): void => {};
+
+  writeValue(obj: any): void {
+    this._currentValue = obj || '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this._disabled = isDisabled;
+  }
+
+  handleInput(event: Event): void {
+    const { value } = event.target as HTMLInputElement;
+    this.onChange(value);
+    this._currentValue = value;
+  }
+
+  onBlur() {
+    this.onTouched(true);
+  }
+
+  get currentValue() {
+    return this._currentValue;
+  }
+
+  get isDisabled(): boolean {
+    return this._disabled;
+  }
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Button } from '../../components/button/button';
+import { InputField, InputFieldVariants } from '../../components/input-field/input-field';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Button],
+  imports: [Button, InputField, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private fb: FormBuilder = inject(FormBuilder);
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  get InputType() {
+    return InputFieldVariants;
+  }
+
   Submit(e: Event) {
     e.preventDefault();
+
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) {
+      console.error('Error', this.loginForm.value);
+      return;
+    }
+
+    alert('login');
   }
 }
